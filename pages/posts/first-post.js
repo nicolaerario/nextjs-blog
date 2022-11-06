4e7fdff6fed4f9fd8833acde2abb7dcb3dfdd2f7
Nicola Erario <nicola.erario@gmail.com>
2022-11-06 19:05:15 +0100

    feat: Use Link component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,3 +1,14 @@
-const FirstPost = () => <h1>First Post</h1>;
+import Link from 'next/link';
+
+const FirstPost = () => {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to Home</Link>
+      </h2>
+    </>
+  );
+};
 
 export default FirstPost;
