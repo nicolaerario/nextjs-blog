75b1f19866d39855dcd5ff2ae7460cce50b8e438
Nicola Erario <nicola.erario@gmail.com>
2022-11-07 19:23:40 +0100

    feat: Add Head component and styles

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,14 +1,25 @@
 import Link from 'next/link';
+import Head from 'next/head';
+import styled from 'styled-components';
 
 const FirstPost = () => {
   return (
-    <>
+    <Container>
+      <Head>
+        <title>First Post</title>
+      </Head>
       <h1>First Post</h1>
       <h2>
         <Link href="/">Back to Home</Link>
       </h2>
-    </>
+    </Container>
   );
 };
 
 export default FirstPost;
+
+const Container = styled.div`
+  max-width: 36rem;
+  padding: 0 1rem;
+  margin: 3rem auto 6rem;
+`;
